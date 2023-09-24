01facc3d4f79af43c94d8ae8103f1146b4581ddc
billkiplagat <bill.kiplagat.7@gmail.com>
2023-09-24 14:35:23 +0300

    Updated SSH project

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,15 @@
 #configure ssh config
-
+file { '/etc/ssh':
+  ensure => directory,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+file { '/etc/ssh/config':
+  owner => 'root',
+  group => 'root',
+  mode  => '0644',
+}
 file_line { 'Turn off passwd auth':
   path => '/etc/ssh/config',
   line => 'PasswordAuthentication no'
