07cfdda29ec5bf867c228d29cf6ca81708c07dd1
billkiplagat <bill.kiplagat.7@gmail.com>
2023-10-02 10:43:03 +0300

    Updated redirect file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,13 @@
+include stdlib
+
 #configure ssh config
-file { '/etc/ssh':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0755',
-}
-file { '/etc/ssh/config':
-  owner => 'root',
-  group => 'root',
-  mode  => '0644',
-}
 file_line { 'Turn off passwd auth':
   path => '/etc/ssh/config',
-  line => 'PasswordAuthentication no'
+  line => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   path => '/etc/ssh/config',
-  line => 'IdentityFile ~/.ssh/school'
+  line => 'IdentityFile ~/.ssh/school',
 
 }
